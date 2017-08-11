84783e11884d230fd87a0e4cb7505cedefcf5d47
Sven Mumenthaler <snail@easytype.ch>
2017-08-11 15:54:22 +0200

    Show 'Request Offer' button for items not in stock

--- x-child-swip/functions.php
+++ x-child-swip/functions.php
@@ -24,3 +24,17 @@ add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
 // =============================================================================
 
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_single_excerpt', 5);
+
+
+
+
+
+
+
+add_filter('woocommerce_get_availability_text', function($text, $product) {
+    if (!$product->is_in_stock()) {
+        $text = '<a href="/en/contact-us/"><button name="request-offer" class="request_quote button alt">Request Offer</button></a>';
+    }
+ 
+    return $text;
+}, 10, 2);
