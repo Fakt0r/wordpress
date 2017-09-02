6ba71aa10a44c43c0871ef13f0e74b142e709a71
Sven Mumenthaler <snail@easytype.ch>
2017-09-02 11:44:03 +0200

    Move Delivery information to processing emails (instead of completed emails)

--- x-child-swip/woocommerce/emails/customer-completed-order.php
+++ x-child-swip/woocommerce/emails/customer-completed-order.php
@@ -23,9 +23,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * @hooked WC_Emails::email_header() Output the email header
  */
-do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+do_action( 'woocommerce_email_header', $email_heading, $email ); 
 
-<p><?php printf( __( "Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) ); ?></p>
+ 
+
+ ?>
+
+<p>
+
+<?php
+
+foreach($order->get_items() as $item) {
+    $product_name = $item['name'];
+}
+
+if ($product_name =='Managed Brainstorming') {
+  printf( __( "Hi there. Your recent order on %s has been completed as your challenge has been closed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) );
+}
+else if ($product_name =='Self-Service Brainstorming') {
+  printf( __( "Hi there. Your recent order on %s has been completed as your challenge has been published. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) );
+}
+else {
+  printf( __( "Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) );
+}
+
+?></p>
 
 <?php
 
@@ -46,38 +68,6 @@ do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, 
  * @hooked WC_Emails::customer_details() Shows customer details
  * @hooked WC_Emails::email_address() Shows email address
  */
-?>
-
-<?php foreach($order->get_items() as $item) {
-    $product_name = $item['name'];
-
-} ?>
-
-
-<div style="color: #636363" class="">
-<h2 style="color:#96588a;display:block;font-family:"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;font-size:18px;font-weight:bold;line-height:130%;margin:16px 0 8px;text-align:left">Delivery</h2>
-
-<?php if ($product_name =='Self-Service Brainstorming') : ?>
-
-Please <a style="color: #15c; font-weight: 700" href="https://www.swip.world/projects/challenges/new" target="_blank">create your own Challenge</a> project now.<br>
-It is important that you specify your problem and what you need as clearly as possible. If you need assistance, please get in touch with us to offer our services.<br>
-Once you’re ready, click on “Publish” to get your challenge going!<br>
-You may also <a style="color: #15c;" href="https://www.swip.world/companies/new" target="_blank">create a company</a> profile page.
-
-
-<?php endif; ?>
-
-<?php if ($product_name =='Managed Brainstorming') : ?>
-
-We will get in touch with you shortly to help prepare the challenge. You may already want to start ahead by <a style="color: #15c; font-weight: 700" href="https://www.swip.world/projects/challenges/new" target="_blank">creating a first draft</a>. <br>
-In the meantime, <strong>please sign up on <a style="color: #15c;" href="https://www.swip.world/companies/new" target="_blank">swip.world</a></strong> using your full name if you haven’t already done so. Optionally, you may also <a style="color: #15c;" href="https://www.swip.world/companies/new" target="_blank">create a company</a> profile page.
-
-
-<?php endif; ?>
-
-
-</div>
-<?php
 
 do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
--- x-child-swip/woocommerce/emails/customer-processing-order.php
+++ x-child-swip/woocommerce/emails/customer-processing-order.php
@@ -25,7 +25,38 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<p><?php _e( "Your order has been received and is now being processed. Your order details are shown below for your reference:", 'woocommerce' ); ?></p>
+<p><?php _e( "Your order has been received and is now being processed. Your order details are shown below for your reference.", 'woocommerce' ); ?></p>
+
+<?php foreach($order->get_items() as $item) {
+    $product_name = $item['name'];
+
+} ?>
+
+
+<div style="color: #636363" class="">
+<h2 style="color:#96588a;display:block;font-family:"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif;font-size:18px;font-weight:bold;line-height:130%;margin:16px 0 8px;text-align:left">Delivery</h2>
+
+<?php if ($product_name =='Managed Brainstorming') : ?>
+
+We will get in touch with you shortly to help prepare the challenge. You may already want to start ahead by <a style="color: #15c; font-weight: 700" href="https://www.swip.world/projects/challenges/new" target="_blank">creating a first draft</a>. <br>
+In the meantime, <strong>please sign up on <a style="color: #15c;" href="https://www.swip.world/companies/new" target="_blank">swip.world</a></strong> using your full name if you haven’t already done so. Optionally, you may also <a style="color: #15c;" href="https://www.swip.world/companies/new" target="_blank">create a company</a> profile page.
+
+
+<?php else: ?>
+
+<?php #if ($product_name =='Self-Service Brainstorming') : ?>
+
+Please <a style="color: #15c; font-weight: 700" href="https://www.swip.world/projects/challenges/new" target="_blank">create your own Challenge</a> project now.<br>
+It is important that you specify your problem and what you need as clearly as possible. If you need assistance, please get in touch with us to offer our services.<br>
+Once you’re ready, click on “Submit” to get your challenge going! After our review, we will publish it.<br>
+You may also <a style="color: #15c;" href="https://www.swip.world/companies/new" target="_blank">create a company</a> profile page.
+
+
+<?php endif; ?>
+
+
+
+</div>
 
 <?php
 
@@ -46,6 +77,8 @@ do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, 
  * @hooked WC_Emails::customer_details() Shows customer details
  * @hooked WC_Emails::email_address() Shows email address
  */
+ 
+ 
 do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
 /**
